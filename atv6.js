f6cd1f18f5ed6c0abb13ba67904b806165df54ee
JOTAGGE <108813623+JOTAGGE@users.noreply.github.com>
2023-10-21 19:36:36 +0000

    New Commit

---
+++ atv6.js
@@ -0,0 +1,4 @@
+var saldo = 0;
+
+var transacoes = Array[deposito, saque, valor, data];
+console.log("Bem-vindo ao Sistema de Conta Bancária! Escolha uma opção: , Depositar,  Sacar Ver Saldo ,Ver Histórico de TransaçõesSair");
---
+++ atv7.js
@@ -0,0 +1,27 @@
+var agencia;
+var numero;
+var tipo;
+var saldo;
+
+const saldo = {
+    saldo: 0,
+
+    get saldo(){
+        return this.saldo;
+    }
+}
+
+const saldo = {
+    saldo: 0,
+
+    set saldo(novoValor){
+        this.saldo = novoValor; 
+    }
+}
+
+const Sacar = {
+
+}
+const Depositar = {
+    
+}
